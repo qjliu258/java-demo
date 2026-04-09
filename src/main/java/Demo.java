cedc8d251613ef2e2ec7c32c9cdfb39adb0e87cf
土豆 <qjliu258@163.com>
2026-04-09 20:37:26 +0800

    update Demo.java

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -2,6 +2,7 @@ package main.java;
 
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("Hello tudou");
+        System.out.println("Hello qinqin");
+        System.out.println("Hello qinqin");
     }
 }

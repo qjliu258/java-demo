52de8ed7a41a55417eabf851ed2bd26496d92a60
liuqinqin0605-maker <liuqinqin0605@gmail.com>
2026-04-09 21:05:08 +0800

    Demo2 add two line qinqin

--- src/main/java/Demo2.java
+++ src/main/java/Demo2.java
@@ -3,5 +3,6 @@ package main.java;
 public class Demo2 {
     public static void main(String[] args) {
         System.out.println("hellow qinqin");
+        System.out.println("hellow qinqin");
     }
 }

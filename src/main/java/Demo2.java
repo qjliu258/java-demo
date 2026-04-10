441ecfd986f4b6e06177cf489a491682f6c64650
土豆 <qjliu258@163.com>
2026-04-10 13:02:45 +0800

    add yellow

--- src/main/java/Demo2.java
+++ src/main/java/Demo2.java
@@ -5,5 +5,6 @@ public class Demo2 {
         System.out.println("hellow qinqin");
         System.out.println("hellow qinqin");
         System.out.println("hellow tudou");
+        System.out.println("yellow");
     }
 }
